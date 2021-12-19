65b6b0b02a8240c5b63a1c6feeb68f488ffc4c53
Joel Dousset <jldc249@gmail.com>
2021-12-19 17:42:55 +0100

    initial commit

---
+++ js/season.js
@@ -0,0 +1,48 @@
+// const date = new Date();
+// const month = date.getMonth();
+// console.log(month);
+
+let month = 1;
+
+switch (month) {
+  case 1:
+    season = 'santaclaus';
+    changeImage();
+    break;  
+  case 2:
+  case 3:
+    season = 'skiing';
+    changeImage();
+    break;
+  case 4:
+  case 5:
+  case 6:
+    season = 'spring';
+    changeImage();
+    break;
+  case 7:
+  case 8:
+  case 9:
+    season = 'summer';
+    changeImage();
+    break;
+  case 10:
+  case 11:
+  case 12:
+    season = 'automn';
+    changeImage();
+    break;
+  default:
+    text = 'What a month!';
+
+    function changeImage() {
+      let imgSource = './img/' + season + '.jpg';
+      let img = document.createElement('img');
+      img.setAttribute('src', imgSource);
+      img.setAttribute('width', '100%');
+      img.setAttribute('height', 'auto');
+      img.setAttribute('alt', 'the current season');
+      let imgNode = document.getElementById('image');
+      imgNode.appendChild(img);
+    }
+}
